bfbf7e578fb6d494f31d1c9abcd6bb732ff34331
Peterhabit <jiseokchoij@gmail.com>
2022-07-07 01:59:28 +0900

    feat
    폴더 추가 및 Navigation.js 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+     
      프로젝트 시작
     </div>
   );
