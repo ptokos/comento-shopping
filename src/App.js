d022caa0011f84ed6c03705509acaea17f7e2065
Peterhabit <jiseokchoij@gmail.com>
2022-07-13 23:38:52 +0900

    chore : App.js에 라우팅 적용 후 라우팅에 해당되는 임시페이지 추가(Home, Product, Basket)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
+import {BrowserRouter,Route, Router } from 'react-router-dom';
+import Home from "./pages/Home"
+import ProductDetail from "./pages/ProductDetail"
+import Basket from "./pages/Basket"
+
 import './App.css';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-     
-     프로젝트 시작
-     action
-     action1
-    </div>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/product/:productId" element={<ProductDetail/>}/>
+        <Route path="/Basket" element={<Basket/>}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
