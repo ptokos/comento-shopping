b1e6cac47eb6d3313dbe35fdccb40fbde4f25e90
Peterhabit <jiseokchoij@gmail.com>
2022-07-20 15:30:35 +0900

    feat: 테마 버튼 클릭 시, mock 상품 리스트 변경 기능 추가

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
-const ThemeButton = ({themeName}) =>{
-    return  <ThemeButtonstyled>{themeName}</ThemeButtonstyled>;
+const ThemeButton = ({ themeName, onClick }) => {
+  return <ThemeButtonstyled onClick={onClick}>{themeName}</ThemeButtonstyled>;
 };
+// styled이지만 div이기에 onClick이 적용된다
+const ThemeButtonstyled = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  width: fit-content;
+  border-radius: 10px;
 
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  padding: 24px 16px;
+  color: white;
+`;
 
-    const ThemeButtonstyled = styled.div`
-    background: rgba(0, 0, 0, 0.5);
-    width: fit-content;
-    border-radius: 10px;
-
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 26px;
-    padding: 24px 16px;
-    color:white;
-    `;
-    
 export default ThemeButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,46 @@ import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { mockTheme1Produdcts } from "../data/mockData";
 import { mockTheme2Produdcts } from "../data/mockData";
 
 const Home = () => {
   // state(상태), 내부적, 리액트에서 변수를 다루는 설명 중 하나이고 하나의 컴포넌트 안에서 사용된다
-  const [products, setProduct] = useState();
+  // 보통 화면이 변할때 state를 사용한다
+  // 다시 랜더링(UI그리는 거)되는 조건 값(state)
+  const [products, setProducts] = useState();
+
+  // 조건에 의해서 실행되는 함수
+  useEffect(() => {
+    setTimeout(() => {
+      setProducts(mockTheme1Produdcts);
+    }, 1000);
+  }, []);
+
+  const onClickThemeButton = (themeId) => {
+    if (themeId === 1) {
+      setProducts(mockTheme1Produdcts);
+    } else if (themeId === 2) {
+      setProducts(mockTheme2Produdcts);
+    }
+    console.log("버튼누름");
+  };
+
   return (
     <div>
       <div>
         <Navigation />
         <ThemeSection>
-          <ThemeButton themeName={"#겨울방한템"} />
+          <ThemeButton
+            themeName={"#겨울방한템"}
+            onClick={() => onClickThemeButton(1)}
+          />
           {/* props, 외부적, 외부에서 값을 넘겨주는 형태  */}
-          <ThemeButton themeName={"#따순머그컵"} />
+          <ThemeButton
+            themeName={"#여름더워요"}
+            onClick={() => onClickThemeButton(2)}
+          />
         </ThemeSection>
         <GrayLine />
       </div>
@@ -23,30 +49,18 @@ const Home = () => {
       <div>
         <ProductSection>
           {/* mockTheme1Produdcts는 배열이고 배열의 값을 map함수를 통해 새로운 값으로 구성 */}
-          {mockTheme1Produdcts.map((product) => (
-            <ProductCard
-              name={product.name}
-              description={product.description}
-              thumbnail={product.thumbnail}
-            />
-          ))}
-          {/* <ProductCard
-            name="비숑 블랙 머그잔"
-            description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다."
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
-          />
-
-          <ProductCard
-            name="가열 보온 티코스터 온열 원터치 컵 받침대"
-            description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다."
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
-          />
-
-          <ProductCard
-            name="벨루즈까사 솜사탕 파스텔 머그 4종 세트"
-            description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그"
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
-          /> */}
+          {products ? (
+            products.map((product) => (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                description={product.description}
+                thumbnail={product.thumbnail}
+              />
+            ))
+          ) : (
+            <div> 테마를 선택해주세요</div>
+          )}
         </ProductSection>
       </div>
     </div>
