56239cc42a2c3936f431e93fa77b3ea40536283b
Peterhabit <jiseokchoij@gmail.com>
2022-07-21 03:30:16 +0900

    fix: ProductCard에 onClick 이벤트 추가, ProductCardStyled 작성

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,32 @@
 import styled from "styled-components";
 
-const ProductCard = ({name, description, thumbnail})=> {
-    return(   
-        <div>
-        <img
-        width="200"
-        src={thumbnail}
-        alt={name}
-        />
-    
-        <NameStyled> {name} </NameStyled>
-        <DesStyled>{description}</DesStyled>
-        </div>
-        );
+const ProductCard = ({ name, description, thumbnail, onClick }) => {
+  return (
+    <ProductCardStyled onClick={onClick}>
+      <img width="200" src={thumbnail} alt={name} />
+
+      <NameStyled> {name} </NameStyled>
+      <DesStyled>{description}</DesStyled>
+    </ProductCardStyled>
+  );
 };
 
+const ProductCardStyled = styled.div`
+  padding: 40px;
+`;
 
-const NameStyled = styled.div` 
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 26px;
+const NameStyled = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
 
-    color: #000000;
+  color: #000000;
 `;
 const DesStyled = styled.div`
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 21px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
 
-    color: #000000;
+  color: #000000;
 `;
 export default ProductCard;
