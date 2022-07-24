54c8da37374bb0877cc0b87bc6df74cdc8af0d3b
Peterhabit <jiseokchoij@gmail.com>
2022-07-24 22:47:25 +0900

    feat: DetailButton 구현(상품설명, 상품후기)

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Navigation from "../components/Navigation";
 import { getProductDetail } from "../data/mockData";
+import DetailButton from "../components/DetailButton";
 
 const ProductDetail = () => {
   let { productId } = useParams();
@@ -13,20 +14,89 @@ const ProductDetail = () => {
     setProduct(result);
   }, []);
 
+  const onClickDetailButton = (buttonId) => {
+    if (buttonId === 1) {
+      <img src={product.img} />;
+    } else if (buttonId === 2) {
+      <img src={product.revivew} />;
+    }
+  };
   return (
     <ProductDetailStyled>
+      <button type="button" onclick="http://localhost:3000/comento-shopping">
+        {" < "}
+      </button>
       <Navigation />
       {product && (
         <>
-          <div>상품 ID : {product.id}</div>
-          <img src={product.thumbnail} width="200" />
-          <div>{product.name}</div>
-          <div>{product.desciotion}</div>
+          <img src={product.thumbnail} width="390" />
+          <NameStyled>{product.name}</NameStyled>
+          <PriceStyled>{product.price}</PriceStyled>
         </>
       )}
+      <GrayLine></GrayLine>
+      <ButtonSection>
+        <DetailButton
+          detailName={"상품 설명"}
+          onClick={() => onClickDetailButton(1)}
+          //   product &&
+          //   product.type === "상품설명" && (
+          //     <>
+          //       <img src={product.img} />
+          //     </>
+          //   )
+          // }
+        />
+        <DetailButton
+          detailName={"상품 후기"}
+          onClick={() => onClickDetailButton(2)}
+          //   onClick={product && product.type === "상품후기" && (
+          //     <>
+          //   <img src={product.revivew} />
+          //     </>
+          // )
+          //   }
+        />
+      </ButtonSection>
+      <GrayLine></GrayLine>
     </ProductDetailStyled>
   );
 };
 
+const ButtonSection = styled.div`
+  display: flex;
+  gap: 0px;
+`;
+
+const GrayLine = styled.div`
+  width: 390px;
+  height: 1px;
+  left: 0px;
+  top: -1px;
+
+  background: #eeeeee;
+`;
+
 const ProductDetailStyled = styled.div``;
+
+const NameStyled = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+
+  margin: 24px 229px 8px 24px;
+
+  color: #000000;
+`;
+
+const PriceStyled = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+
+  margin: 8px 303px 24px 24px;
+
+  color: #000000;
+`;
+
 export default ProductDetail;
