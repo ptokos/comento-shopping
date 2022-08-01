b62faaaf55786bab1b28790e02d2c0bace8ef993
Peterhabit <jiseokchoij@gmail.com>
2022-08-02 02:13:06 +0900

    feat : 장바구니 UI components 작성

---
+++ src/components/BasketItem.jsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const BasketItem = ({ thumbnail, name, price, onClickRemoveButton }) => {
+  return (
+    <>
+      <BasketItemStyled>
+        <RowLayout>
+          <RowLayout>
+            <ProductImage styled={{ backgroundImage: `url(${thumbnail})` }} />
+            <ColumLayout>
+              <ProductName>{name}</ProductName>
+              <div>{price}원</div>
+            </ColumLayout>
+          </RowLayout>
+          <CloseButton onClick={onClickRemoveButton}>X</CloseButton>
+        </RowLayout>
+      </BasketItemStyled>
+      <GrayLine />
+    </>
+  );
+};
+
+export default BasketItem;
+
+const BasketItemStyled = styled.div`
+  padding: 16px;
+  margin: 10px 0px;
+`;
+
+const RowLayout = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+`;
+
+const ColumLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ProductImage = styled.div`
+  width: 120px;
+  height: 100px;
+  background-size: cover;
+  background-position: center;
+`;
+
+const CloseButton = styled.div`
+  cursor: pointer;
+`;
+
+const ProductName = styled.div`
+  font-size: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 21px;
+  letter-spacing: -0.01em;
+  color: #000000;
+`;
+
+const GrayLine = styled.div`
+  height: 1px;
+  width: 100%;
+  background: #eeeeee;
+`;
