4792776c7dea8dbfd7e2c4cdb294525c530f52be
Peterhabit <jiseokchoij@gmail.com>
2022-08-02 02:28:38 +0900

    feat : 화면에 장바구니에 담긴 정보 출력

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,12 +1,37 @@
 import * as storage from "../utils/storage";
 import Navigation from "../components/Navigation";
 import styled from "styled-components";
+import BasketItem from "../components/BasketItem";
+import { useEffect, useState } from "react";
 
 const Basket = () => {
-  console.log(storage.getBasketItems());
+  const [basketItems, setBasketItems] = useState();
+
+  useEffect(() => {
+    const items = storage.getBasketItems();
+    setBasketItems(items);
+    console.log(items);
+  }, []);
+
+  //2. UI 구현
+  const onClickRemoveButton = () => {
+    alert("삭제");
+  };
+
   return (
     <BasketStyled>
       <Navigation name="장바구니" hasBack={true} />
+
+      {basketItems &&
+        basketItems.map((item) => (
+          <BasketItem
+            key={item.id} //map을 돌때 key를 넣어주어야 한다
+            thumbnail={item.thumbnail}
+            name={item.name}
+            price={item.price}
+            onClickRemoveButton={onClickRemoveButton}
+          />
+        ))}
     </BasketStyled>
   );
 };
